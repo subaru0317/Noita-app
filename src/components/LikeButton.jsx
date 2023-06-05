e35b41c4cfc016b19e3de0938ca05621e34313f9
subaru0317 <m.kurakura.s@icloud.com>
2023-06-06 00:36:13 +0900

    いいねボタン高速clickでLike数がおかしくなる問題解決．また，いいねボタンの表記を変更

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { auth, db } from '../firebase';
 import { FaHeart } from "react-icons/fa";
-import { IconButton, Flex, Text } from "@chakra-ui/react";
+import { Flex, Box, Button } from "@chakra-ui/react";
 import { increment, updateDoc, doc, setDoc, getDoc, deleteDoc, serverTimestamp, onSnapshot } from "firebase/firestore";
+
 
 const LikeButton = ({imageDocData}) => {
   const [likeCount, setLikeCount] = useState(imageDocData.likeCount);
@@ -26,35 +27,46 @@ const LikeButton = ({imageDocData}) => {
     return () => unsubscribe();
   }, [imageDocData]);
 
+  const [processing, setProcessing] = useState(false);
+
   const handleLikeButtonClick = async () => {
+    setProcessing(true);
+  
     const loginUserId = auth.currentUser.uid;
     const likesRef = doc(db, "users", imageDocData.userId, "images", imageDocData.fileId, "likes", loginUserId);
     const imageRef = doc(db, "users", imageDocData.userId, "images", imageDocData.fileId);
-
+  
     const docSnap = await getDoc(likesRef);
-
+  
     if (docSnap.exists()) {
       setLiked(false);
       setLikeCount(likeCount - 1);
-      deleteDoc(likesRef).then(() => updateDoc(imageRef, { likeCount: increment(-1) }));
+      await deleteDoc(likesRef);
+      await updateDoc(imageRef, { likeCount: increment(-1) });
     } else {
       setLiked(true);
       setLikeCount(likeCount + 1);
-      setDoc(likesRef, { likedAt: serverTimestamp() }, { merge: true }).then(() => updateDoc(imageRef, { likeCount: increment(1) }));
+      await setDoc(likesRef, { likedAt: serverTimestamp() }, { merge: true });
+      await updateDoc(imageRef, { likeCount: increment(1) });
     }
+  
+    setProcessing(false);
   };
+  
   
   return (
     <Flex alignItems="center">
-      <IconButton
-        icon={<FaHeart color={liked ? "red" : "black"} />}
-        aria-label="Like"
+      <Button
+        leftIcon={<FaHeart color={liked ? "red" : "black"} />}
+        aria-label='Like'
         onClick={handleLikeButtonClick}
         mt={2}
-      />
-      <Text ml="2" fontSize="xl">
-        {likeCount}
-      </Text>
+        isDisabled={processing}
+      >
+        <Box mt={-1} fontSize="19px">
+          {likeCount}
+        </Box>
+      </Button>
     </Flex>
   )
 }
