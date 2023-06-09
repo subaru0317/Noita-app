8426e8121ea84aab489b2e451f4f73e829c066b8
subaru0317 <m.kurakura.s@icloud.com>
2023-06-10 02:16:32 +0900

    FilterとVideoCardを分割する線を追加

--- src/routes/videopage.jsx
+++ src/routes/videopage.jsx
@@ -1,14 +1,16 @@
 import VideoList from '../components/VideoList';
 import FilterModal from '../components/FilterModal';
 import { useState } from "react";
-import { Divider } from "@chakra-ui/react";
+import { Divider, Box } from "@chakra-ui/react";
 const VideoPage = () => {
   const [selectedSpells, setSelectedSpells] = useState([]);
   const [filterMode, setFilterMode] = useState("OR");
   return (
     <>
       <FilterModal setSelectedSpells={setSelectedSpells} setFilterMode={setFilterMode}/>
-      <Divider orientation='horizontal'/>
+      <Box mt={4} mb={8} px={12}>
+        <Divider orientation='horizontal'/>
+      </Box>
       <VideoList selectedSpells={selectedSpells} filterMode={filterMode}/>
     </>
   );
