d090fad60a3149de0f45415d73783425a8499926
subaru0317 <m.kurakura.s@icloud.com>
2023-06-21 21:01:49 +0900

    高速化のため，SpellIconのloadlazyを削除

--- src/components/SpellIcon.jsx
+++ src/components/SpellIcon.jsx
@@ -1,6 +1,59 @@
+// import { memo } from "react";
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import 'react-lazy-load-image-component/src/effects/blur.css';
+
+// const SpellIcon = memo(({ spell, bg="#595959", size="35px" }) => {
+//   let borderColor;
+//   switch (spell.type) {
+//     case 'projectile':
+//       borderColor = "#5A2323";
+//       break;
+//     case 'static_projectile':
+//       borderColor = "#8D3F18";
+//       break;
+//     case 'passive':
+//       borderColor = "#212F26";
+//       break;
+//     case 'utility':
+//       borderColor = "#7B2A74";
+//       break;
+//     case 'projectile_modifier':
+//       borderColor = "#2D3A72";
+//       break;
+//     case 'material':
+//       borderColor = "#356F44";
+//       break;
+//     case 'multicast':
+//       borderColor = "#1C6D73";
+//       break;
+//     case 'other':
+//       borderColor = "#714B33";
+//       break;
+//     default:
+//       borderColor = "#714B33";
+//   }
+
+//   return (
+//       <LazyLoadImage 
+//         src={spell.path}
+//         alt={spell.name}
+//         effect="blur"
+//         width={size}
+//         height={size}
+//         style={{
+//           border: `3px solid ${borderColor}`,
+//           borderRadius: "2px",
+//           backgroundColor: bg,
+//         }}
+//       />
+//   );
+// });
+
+// export default SpellIcon;
+
+
 import { memo } from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { Image } from "@chakra-ui/react";
 
 const SpellIcon = memo(({ spell, bg="#595959", size="35px" }) => {
   let borderColor;
@@ -34,18 +87,17 @@ const SpellIcon = memo(({ spell, bg="#595959", size="35px" }) => {
   }
 
   return (
-      <LazyLoadImage 
-        src={spell.path}
-        alt={spell.name}
-        effect="blur"
-        width={size}
-        height={size}
-        style={{
-          border: `3px solid ${borderColor}`,
-          borderRadius: "2px",
-          backgroundColor: bg,
-        }}
-      />
+    <Image
+      src={spell.path}
+      alt={spell.name}
+      boxSize={size}
+      loading="lazy"
+      borderRadius="2px"
+      borderStyle="solid"
+      borderColor={borderColor}
+      borderWidth="3px"
+      bg={bg}
+    />
   );
 });
 
