b4a201882cd7bca7fa730cc8f03d5fd280689c85
subaru0317 <m.kurakura.s@icloud.com>
2023-06-28 16:45:36 +0900

    mypageの中身を一部作成

--- src/routes/favoritepage.jsx
+++ src/routes/favoritepage.jsx
@@ -1,10 +1,70 @@
+import { useState, useEffect } from "react";
+import VideoCardList from '../components/VideoCardList';
+import FilterModal from '../components/FilterModal';
 import SpacingDivider from "../components/SpacingDivider";
-import { Text } from "@chakra-ui/react";
-const FavoritePage = () => {
-  return (
-    <p>FavoritePage</p>
+import SelectedVideoTag from "../components/SelectedVideoTag";
+import SearchButton from "../components/SearchButton";
+import ModeSelector from "../components/ModeSelector";
+import DisplaySpellIcons from "../components/DisplaySpellIcons";
+import { Box, VStack, HStack, Text, Heading } from '@chakra-ui/react';
 
+const VideoListPage = () => {
+  const [selectedSpells, setSelectedSpells] = useState([]);
+  const [selectedSpellsMode, setSelectedSpellsMode] = useState("OR");
+  const [videoTag, setVideoTag] = useState([]);
+  const [videoTagMode, setVideoTagMode] = useState("OR");
+  const [search, setSearch] = useState(false);
+  const [firstRender, setFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (firstRender) {
+      setSearch(true);
+      setFirstRender(false);
+    }
+  }, [firstRender]);
+
+  return (
+    <>
+      <Box mt={5} ml={5}>
+        <Heading as='h2' size='xl'> Favorite </Heading>
+      </Box>
+      <SpacingDivider />
+      <VStack mt={5} ml={5} align="start" spacing={4}>
+        <HStack>
+          <Box flexShrink={0}>
+            <FilterModal setSelectedSpells={setSelectedSpells} />
+          </Box>
+          <VStack align="left" flexGrow={1}>
+            <DisplaySpellIcons spells={selectedSpells} />
+            <ModeSelector mode={selectedSpellsMode} setMode={setSelectedSpellsMode} description={"spells"}/>
+          </VStack>
+        </HStack>
+        <HStack>
+          <Box ml="114px">
+            <Text>Tags: </Text>
+          </Box>
+          <VStack align="left" flexGrow={1}>
+            <SelectedVideoTag videoTag={videoTag} setVideoTag={setVideoTag} />
+            <ModeSelector mode={videoTagMode} setMode={setVideoTagMode} description={"tags"}/>
+          </VStack>
+        </HStack>
+        <Box display="flex" justifyContent="flex-end" width="95%">
+          <SearchButton setSearch={setSearch} />
+        </Box>
+      </VStack>
+      <SpacingDivider />
+      <VideoCardList
+        videoCardMode={"normal"}
+        fetchMode={"userLiked"}
+        selectedSpells={selectedSpells}
+        selectedSpellsMode={selectedSpellsMode}
+        videoTag={videoTag}
+        videoTagMode={videoTagMode}
+        search={search}
+        setSearch={setSearch}
+      />
+    </>
   );
 }
 
-export default FavoritePage;
+export default VideoListPage;
--- src/routes/mypage.jsx
+++ src/routes/mypage.jsx
@@ -1,7 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { Box, Button, Image, Input, VStack, Stack, Text, Flex, Heading } from "@chakra-ui/react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import SpacingDivider from "../components/SpacingDivider";
+
 const MyPage = () => {
+  const { userId } = useParams();
+  const [userName, setUserName] = useState("");
+  const [userIcon, setUserIcon] = useState("");
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingIcon, setIsEditingIcon] = useState(false);
+
+  useEffect(() => {
+    const db = getFirestore();
+    const fetchData = async () => {
+      const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setUserName(docSnap.data().name);
+        setUserIcon(docSnap.data().icon);
+      }
+    };
+
+    fetchData();
+  }, [userId]);
+
+  const updateUserInfo = async (field, value) => {
+    const db = getFirestore();
+    const docRef = doc(db, "users", getAuth().currentUser.uid);
+    await updateDoc(docRef, { [field]: value });
+
+    if (field === "name") {
+      setUserName(value);
+      setIsEditingName(false);
+    } else if (field === "icon") {
+      setUserIcon(value);
+      setIsEditingIcon(false);
+    }
+  };
+
   return (
-    <p>MyPage</p>
+    <>
+    <Box mt={5} ml={5}>
+      <Heading as='h2' size='xl'> My Page </Heading>
+    </Box>
+    <SpacingDivider />
+    <Flex direction="column" align="center" justify="center" m={5}>
+      <Box mb={5}>
+        <Image borderRadius="full" boxSize="150px" src={userIcon} alt={userName} />
+        {isEditingIcon && (
+          <Input
+            value={userIcon}
+            onChange={(e) => setUserIcon(e.target.value)}
+            onBlur={() => updateUserInfo("icon", userIcon)}
+          />
+        )}
+        <Button onClick={() => setIsEditingIcon(!isEditingIcon)}>
+          Edit Icon
+        </Button>
+      </Box>
+      <Box>
+        {!isEditingName ? (
+          <Stack spacing={1} align="center">
+            <Text fontSize="2xl">{userName}</Text>
+            <Button size="sm" onClick={() => setIsEditingName(true)}>
+              Edit Name
+            </Button>
+          </Stack>
+        ) : (
+          <VStack spacing={2}>
+            <Input
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              onBlur={() => updateUserInfo("name", userName)}
+            />
+            <Button size="sm" onClick={() => setIsEditingName(false)}>
+              Done
+            </Button>
+          </VStack>
+        )}
+      </Box>
+    </Flex>
+    </>
   );
-}
+};
 
 export default MyPage;
