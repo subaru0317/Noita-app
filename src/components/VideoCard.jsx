87db13a7d1cfb0d9aad552760cdd2d4721862d83
subaru0317 <m.kurakura.s@icloud.com>
2023-09-25 21:32:29 +0900

    動画の詳細ページで日付がNAN表示だったのを正しく表示されるように修正．ただし，場当たり的な解決．詳しくはコードに直接コメントしてます

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState, useMemo, memo } from 'react';
-import { Link } from "react-router-dom";
-import { Box, Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Link } from 'react-router-dom';
+import { Box, Flex, Text, Wrap, WrapItem } from '@chakra-ui/react';
 import { auth, storage } from '../firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
-import { onAuthStateChanged } from "firebase/auth";
-import LikeButton from "./LikeButton";
+import { onAuthStateChanged } from 'firebase/auth';
+import LikeButton from './LikeButton';
 import SpellIcon from './SpellIcon';
-import VideoTagItem from "./VideoTagItem";
+import VideoTagItem from './VideoTagItem';
 
 const VideoCard = memo(({ imageDocData, isLinkActive = true }) => {
-  console.log("imageDocData: ", imageDocData);
-  const date = new Date(imageDocData.timestamp);
+  let dateString;
+  // ここがわけわからん
+  // 動画が，
+  // 一覧表示の画面のとき(isLinkActive = true)ではtoDate()するとError．new DateならOK
+  // 詳細表示の画面のとき(isLinkActive = false)ではnew DateするとError．toDate()ならOK
+  // isLinkActiveで分岐させたが，根本的な原因が不明．なんやこれ
+  // memoでDateオブジェクトが残ってたりするのか？わからーーーん
+  // こうすればとりあえず正しく日付が表示されるようになってのでヨシ！
+  if (isLinkActive) {
+    const date = new Date(imageDocData.timestamp);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    
+    dateString = `${year}/${month}/${day}`;
+  } else {
+    const timestamp = imageDocData.timestamp;
+    const year = timestamp.toDate().getFullYear();
+    const month = String(timestamp.toDate().getMonth() + 1).padStart(2, '0');
+    const day = String(timestamp.toDate().getDate()).padStart(2, '0');
 
-  const year = date.getFullYear();
-  const month = ("0" + (date.getMonth() + 1)).slice(-2); // JavaScriptの月は0から始まるため1を追加します
-  const day = ("0" + date.getDate()).slice(-2);
-
-  const dateString = `${year}/${month}/${day}`;
-
+    dateString = `${year}/${month}/${day}`;
+  }
+    
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -25,30 +40,30 @@ const VideoCard = memo(({ imageDocData, isLinkActive = true }) => {
       setIsLoggedIn(!!user);
     });
 
-    // Unsubscribe the listener when the component is unmounted
     return () => unsubscribe();
   }, []);
 
-  const hoverAndClickStyles = isLinkActive ? {
-    transition: "transform 0.3s, box-shadow 0.3s", 
-    _hover: { 
-      transform: "scale(1.05)",
-      boxShadow: "0 0 10px #00BFFF",
+  const hoverAndClickStyles = isLinkActive
+    ? {
+      transition: 'transform 0.3s, box-shadow 0.3s',
+      _hover: {
+        transform: 'scale(1.05)',
+        boxShadow: '0 0 10px #00BFFF',
+      },
     }
-  } : {};
+    : {};
 
   const [videoUrl, setVideoUrl] = useState(null);
 
   useEffect(() => {
     const videoRef = ref(storage, imageDocData.filePath);
 
-    // 動画ファイルのダウンロードURLを取得
     getDownloadURL(videoRef)
       .then((url) => {
-        setVideoUrl(url); // ダウンロードURLを設定
+        setVideoUrl(url);
       })
       .catch((error) => {
-        console.error("動画のダウンロードに失敗しました．", error);
+        console.error('動画のダウンロードに失敗しました．', error);
       });
   }, [imageDocData.filePath]);
 
@@ -63,9 +78,7 @@ const VideoCard = memo(({ imageDocData, isLinkActive = true }) => {
       )}
       <Box p="6">
         <Wrap spacing={0} justify="start">
-          <Text ab='b'>
-            {imageDocData.videoTitle}
-          </Text>
+          <Text as="b">{imageDocData.videoTitle}</Text>
         </Wrap>
         <Wrap spacing={0} justify="start">
           {imageDocData.wandSpells.map((spellName, index) => (
@@ -82,19 +95,15 @@ const VideoCard = memo(({ imageDocData, isLinkActive = true }) => {
           ))}
         </Wrap>
         <Flex justifyContent="space-between" alignItems="center" mt="2">
-          <Text color="#747474">
-            {`${dateString}`}
-          </Text>
-          <LikeButton imageDocData={imageDocData} isLoggedIn={isLoggedIn}/>
+          <Text color="#747474">{dateString}</Text>
+          <LikeButton imageDocData={imageDocData} isLoggedIn={isLoggedIn} />
         </Flex>
       </Box>
     </Box>
   );
 
   return isLinkActive ? (
-    <Link to={videoDetailUrl}>
-      {cardContent}
-    </Link>
+    <Link to={videoDetailUrl}>{cardContent}</Link>
   ) : (
     <div>{cardContent}</div>
   );
