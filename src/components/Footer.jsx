99bd0b65e657f23b61c098ab0ba17b5c42bd4a5d
subaru0317 <m.kurakura.s@icloud.com>
2023-11-07 10:49:33 +0900

    画面最下部にFooterを固定するように変更

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import { Box, Container, Flex, Link, Text } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Box as="footer" bg="blue.400" color="white" py="8" position="fixed" bottom="0" left="0" right="0">
+      <Container maxW="container.xl">
+        <Flex justify="space-between" align="center">
+          <Box>
+            <Text fontSize="xl" fontWeight="bold">
+              Noita - Your Only Wand
+            </Text>
+            <Text mt="2">© 2023. All rights reserved.</Text>
+          </Box>
+
+          <Box>
+            <Text mb="2">Follow us on:</Text>
+            <Flex>
+              <Link mr="4" href="#" target="_blank" rel="noopener noreferrer">
+                Twitter
+              </Link>
+              <Link href="#" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </Link>
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
