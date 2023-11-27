60eb59c252527f6549dd653a60af8522f3d9a0c6
subaru0317 <m.kurakura.s@icloud.com>
2023-11-27 21:54:38 +0900

    Buy me a coffeeボタン

---
+++ src/components/BuyMeACoffeeButton.jsx
@@ -0,0 +1,13 @@
+const BuyMeACoffeeButton = () => {
+  return (
+    <a href="https://www.buymeacoffee.com/subaru03" target="_blank" rel="noopener noreferrer">
+      <img
+        style={{ height: "50px" }}
+        alt="Buy me a Coffee Widget"
+        src="icons/yellow-button.webp"
+      />
+    </a>
+  );
+}
+
+export default BuyMeACoffeeButton;
